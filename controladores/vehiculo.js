63ba2eb6bb565253611e52ca4c51ac5559d224d7
walbyn <walbyngonzalez@gmail.com>
2026-02-22 17:09:26 -0600

    Se creó el método de eliminar vehiculos

--- controladores/vehiculo.js
+++ controladores/vehiculo.js
@@ -33,7 +33,21 @@ const editarVehiculo = async (req, res) => {
     }
 };
 
+const eliminarVehiculo = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedVehiculo = await Vehiculo.findByIdAndDelete(id);
+        if (!deletedVehiculo) {
+            return res.status(404);
+        }
+        res.status(200);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
 module.exports = {
     crearVehiculo,
-    editarVehiculo
+    editarVehiculo,
+    eliminarVehiculo
 };
