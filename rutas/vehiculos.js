71879ffc9c672ab6360f8e5c8332537375dc3582
Seidy <seidyalanis@gmail.com>
2026-02-22 17:31:22 -0600

    Implementación de la ruta POST para crear vehículo.

--- rutas/vehiculos.js
+++ rutas/vehiculos.js
@@ -2,3 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { crearVehiculo } = require("../controladores/vehiculo");
+
+
+
+// Ruta para crear un nuevo vehículo
+router.post('/vehiculo', crearVehiculo)
+
+
+
+
