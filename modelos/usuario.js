cb55e489dd27bb6cca421cf33371da0848ddd6bb
walbyn <walbyngonzalez@gmail.com>
2026-02-26 10:11:43 -0600

    Se crea el modelo de usuarios

---
+++ modelos/usuario.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const usuarioSchema = new mongoose.Schema({
+
+    nombre: {
+        type: String,
+        required: true
+    },
+
+    primerApellido: {
+        type: String,
+        required: true  
+    },
+
+    segundoApellido: {
+        type: String,
+        required: true
+    },
+    
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
