abaf5bcb6182fda7ac278100cd3036910524f043
walbyn <walbyngonzalez@gmail.com>
2026-03-08 13:35:51 -0600

    Se modifica (agregan nuevos atributos)

--- controladores/vehiculos/crearVehiculo.js
+++ controladores/vehiculos/crearVehiculo.js
@@ -2,21 +2,38 @@ const Vehiculo = require('../../modelos/vehiculo');
 
 const crearVehiculo = async (req, res) => {
     try {
+
+        if (!req.file) {
+            return res.status(400).json({
+                message: "Debe subir una imagen"
+            });
+        }
+
+        if (!req.usuario) {
+            return res.status(401).json({
+                message: "Usuario no autenticado"
+            });
+        }
+
         const vehiculo = new Vehiculo({
             marca: req.body.marca,
             modelo: req.body.modelo,
             anno: req.body.anno,
             precio: req.body.precio,
             imagen: req.file.filename,
-            usuario: req.usuario._id
+            combustible: req.body.combustible,
+            color: req.body.color,
+            transmision: req.body.transmision,
+            condicion: req.body.condicion,
+            usuario: req.usuario.id
         });
 
-        // Guardar el vehículo en la BD y retornar el vehículo creado
         const vehiculoCreado = await vehiculo.save();
+
         res.status(201).json(vehiculoCreado);
-        
 
     } catch (error) {
+        console.error("ERROR CREAR VEHICULO:", error);
         res.status(400).json({ message: error.message });
     }
 };
