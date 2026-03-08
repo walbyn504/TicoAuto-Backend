b5d3548a29868e35d1c517bb5132f7f3923c5ba4
walbyn <walbyngonzalez@gmail.com>
2026-03-07 23:34:37 -0600

    Se crea el metodo (verificarTokenOpcional), donde valida que si no hay token (usuario = null) y si hay token (guardamos la informacion del usuario)

--- controladores/autenticacion.js
+++ controladores/autenticacion.js
@@ -113,9 +113,33 @@ const verificarToken = (req, res, next) => {
     }
 };
 
+const verificarTokenOpcional = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        req.usuario = null;
+        return next();
+    }
+
+    try {
+        // Intentamos verificar el token usando la clave secreta
+        const tokenDescifrado = jwt.verify(token, SECRET_KEY);
+
+        // Si el token es válido, guardamos la info del usuario en req.usuario
+        req.usuario = tokenDescifrado; 
+    } catch (error) {
+        // Si el token es inválido o expiró (invitado)
+        req.usuario = null;
+    }
+
+    next();
+};
+
 
 module.exports = {
     registrarUsuario,
     generarToken,
-    verificarToken
+    verificarToken,
+    verificarTokenOpcional
 };
